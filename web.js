785c119f7943fe3b3c38634cc6547916c743816a
Ubuntu <ubuntu@ip-172-31-36-138.us-west-2.compute.internal>
2013-07-11 14:03:01 +0000

    web.js modified

--- web.js
+++ web.js
@@ -6,14 +6,14 @@ app.get('/', function(request, response) {
    // response.send("hello");
     var buf = new Buffer(50);
     var fs = require('fs');
-/*    var data = fs.readFileSync('index.html','utf8');
-    buf.write(data,'utf-8');
-    response.send(buf.toString('utf-8');*/
-    fs.readFileSync("index.html",function (err, data) {
+/*    var data = fs.readFileSync('index.html','utf8');*/
+    buf.write("Hello Everyone",'utf-8');
+    response.send(buf.toString('utf-8');
+/*    fs.readFileSync("index.html",function (err, data) {
         if (err) throw err;
         buf.write(data,'utf-8');
         response.send(buf.toString('utf-8'));
-    });
+    });*/
 });
 
 var port = process.env.PORT || 5000;
