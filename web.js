7688cd567a8f967b45e91f2a9d004f551f10b5c8
Ubuntu <ubuntu@ip-172-31-36-138.us-west-2.compute.internal>
2013-07-11 14:38:27 +0000

    web.js modified

--- web.js
+++ web.js
@@ -5,15 +5,20 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var buf = new Buffer(27);
     var fs = require("fs");
-/*    var data = fs.readFileSync("index.html","utf8");
-    buf.write(data,"utf-8");
-    response.send(buf.toString("utf-8"));*/
     var data;
-    fs.readFileSync("index.html", function (err,data) {
+    try {
+        data = fs.readFileSync("index.html");
+    } catch (e) {
+        response.send("Error: Cannot open file!");
+    }
+/*    var data = fs.readFileSync("index.html","utf8");*/
+    buf.write(data);
+    response.send(buf.toString("utf-8"));*/
+/*    fs.readFileSync("index.html", function (err,data) {
         if (err) throw err;
         buf.write(data,"utf-8");
         response.send(buf.toString("utf-8"));
-    });
+    });*/
 });
 
 var port = process.env.PORT || 5000;
